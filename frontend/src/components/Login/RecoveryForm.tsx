fbd5d1f170c9280a8418c214c0d5efbf2542b26b
ChocoCodes <chocojhong999@gmail.com>
2025-06-16 00:21:43 +0800

    Chore/front: RecoveryForm Step 2

--- frontend/src/components/Login/RecoveryForm.tsx
+++ frontend/src/components/Login/RecoveryForm.tsx
@@ -38,12 +38,13 @@ export const RecoveryForm = ({ onBack, onSubmit, onUpdate }: RecoveryProps) => {
         <form
             action='submit'
             onSubmit={ handleSubmit }
-            className='flex flex-col gap-4 py-3'
+            className='flex flex-col gap-6 py-3'
         >
             <select 
                 name="question"
                 value={ recovery.question } 
                 onChange={ handleChange }
+                className='h-[3rem] bg-bg-input text-ph-gray px-3 rounded-md'
                 required
             >
                 <option value="">Select a recovery question</option>
@@ -60,8 +61,26 @@ export const RecoveryForm = ({ onBack, onSubmit, onUpdate }: RecoveryProps) => {
                 name="answer"
                 value={ recovery.answer }
                 onChange={ handleChange }
+                className='bg-bg-input placeholder-ph-gray h-[3rem] focus:outline-none rounded-md px-3'
+                placeholder='Type your answer here...'
                 required
             />
+            <div className="flex justify-between items-center">
+                <button 
+                    onClick={ onBack } 
+                    type='button' 
+                    className='mr-10 flex items-center gap-1 text-xl hover:scale-110 font-medium hover:cursor-pointer transition-transform duration-200 ease-in-out'
+                >
+                    <BiSolidChevronLeftSquare className='text-4xl text-[#1A1A1A]'/>
+                    Back
+                </button>
+                <button 
+                    type='submit'
+                    className='bg-[#1A1A1A] text-white px-4 py-2 rounded-md hover:cursor-pointer hover:bg-black'
+                >
+                    Register
+                </button>
+            </div>
         </form>
     )
 }
